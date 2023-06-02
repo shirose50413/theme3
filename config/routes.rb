71bff414af7c20eff7fa459ce325a827cbf04e04
shirose50413 <132430261+shirose50413@users.noreply.github.com>
2023-06-02 16:23:58 +0000

    [Add User] 4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   devise_for :users
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as: 'about'
